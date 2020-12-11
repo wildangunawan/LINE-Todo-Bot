ebe016dc519dcca72e92bcb929aa880ff199ad52
Wildan Gunawan <gwildan50@gmail.com>
2020-12-11 21:51:39 +0800

    idk man 2

--- public/index.php
+++ public/index.php
@@ -168,19 +168,19 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                             } else {
                                 $counter = count($tugas);
                                 $flexTemplate = file_get_contents("pending.json"); 
-                                $flexTemplate = json_decode($flexTemplate, true);
+                                // $flexTemplate = json_decode($flexTemplate, true);
 
-                                $flexTemplate['header']['contents'][1]['text'] = "Ada $counter tugas yang pending";
+                                // $flexTemplate['header']['contents'][1]['text'] = "Ada $counter tugas yang pending";
 
-                                foreach ($tugas as $id => $detail) {
-                                    $flexTemplate['body']['contents']['contents'][] = [
-                                            "type" => "text",
-                                            "text" => $id . ". " . $detail,
-                                            "color" => "#8C8C8C",
-                                            "size" => "sm",
-                                            "wrap" => true
-                                    ];
-                                }
+                                // foreach ($tugas as $id => $detail) {
+                                //     $flexTemplate['body']['contents']['contents'][] = [
+                                //             "type" => "text",
+                                //             "text" => $id . ". " . $detail,
+                                //             "color" => "#8C8C8C",
+                                //             "size" => "sm",
+                                //             "wrap" => true
+                                //     ];
+                                // }
 
                                 $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
                                     'replyToken' => $event['replyToken'],
@@ -188,7 +188,7 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                                         [
                                             'type'     => 'flex',
                                             'altText'  => 'Lihat daftar tugas',
-                                            'contents' => json_decode(json_encode($flexTemplate))
+                                            'contents' => json_decode($flexTemplate)
                                         ]
                                     ],
                                 ]);
